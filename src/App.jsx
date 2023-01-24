696595d4e8e936ddb2dbbd405a9ee2cdac82a7cd
deepakkirito <52093112+deepakkirito@users.noreply.github.com>
2023-01-24 21:16:21 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
   return (
     <BrowserRouter>
     <div className="App">
-      <div className="Overflow">
+      <div>
       <Routes>
         <Route path='/' element={<HomePage />} ></Route>
         <Route path='/Courses' element={<CoursesPage />} ></Route>
