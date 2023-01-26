314127dbe92f8043fcb60af0012a39f9ae0f4d16
deepakkirito <52093112+deepakkirito@users.noreply.github.com>
2023-01-26 11:42:23 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Footer from './Components/Footer'
 import Home from './Components/Home'
 import CoursesPage from './Components/CoursesPage'
 import EnrollPage from './Components/EnrollPage'
-import AboutPage from './Components/AboutPage'
+import InstructorPage from './Components/InstructorPage'
 import LoginPage from './Components/LoginPage'
 import StudentPage from './Components/StudentPage'
 
@@ -20,7 +20,7 @@ function App() {
         <Route path='/' element={<Home />} ></Route>
         <Route path='/Courses' element={<CoursesPage />} ></Route>
         <Route path='/Enroll' element={<EnrollPage />} ></Route>
-        <Route path='/About' element={<AboutPage />} ></Route>
+        <Route path='/About' element={<InstructorPage />} ></Route>
         <Route path='/Login' element={<LoginPage />} ></Route>
         <Route path='/Student' element={<StudentPage />} ></Route>
       </Routes>
