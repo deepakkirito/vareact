353e02a117f32a4592529da9883d827fa1576b7b
deepakkirito <52093112+deepakkirito@users.noreply.github.com>
2023-02-07 16:51:13 +0530

    Update CoursesPage.jsx

--- src/Components/CoursesPage.jsx
+++ src/Components/CoursesPage.jsx
@@ -3,7 +3,7 @@ import { useEffect } from 'react';
 import { useState } from 'react';
 import classes from '../Styles/CoursesPage.module.scss';
 
-const baseUrl = 'https://liveserver.glitch.me/';
+const baseUrl = 'https://liveserver-x7b0.onrender.com/';
 
 
 function CoursesPage() {
